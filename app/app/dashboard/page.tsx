103c76153eb19790dc83702ae1830c7185bdc8a0
sidxt <srisiddhaarthp@gmail.com>
2024-04-24 15:39:46 +0530

    test

--- app/app/dashboard/page.tsx
+++ app/app/dashboard/page.tsx
@@ -4,46 +4,79 @@ import {useSession } from 'next-auth/react'
 import {useRouter} from 'next/navigation'
 import {useState,useEffect} from 'react'
 import axios from 'axios'
-
+import Home from '../page'
 const HomePage = () => {
-    const searchParams = useSearchParams();
-    const search = searchParams.get('v');
-    const session = useSession()
-    const router=useRouter()
-    const [validation,set_validation]=useState()
-    //console.log("it is "+search)
+  const searchParams = useSearchParams();
+  const search = searchParams.get('v');
+  const session = useSession();
+  const router = useRouter();
+  const [validation, set_validation] = useState();
+  const [links, set_links] = useState([]);
+  const [images, set_images] = useState([]);
+  const [loading, set_loading] = useState(false);
+  
 
-      const validate=async()=>{
-        const response = await axios.post("http://localhost:8000/api/guard-the-fact",{url:search})
-        set_validation(response.data.final)
-      }
+  const validate = async () => {
+      //set_loading(true);
+      const response = await axios.post("http://localhost:8000/api/guard-the-fact", { url: search });
+      console.log(response.data)
+      //set_validation(response.data.final);
+      //set_links(response.data.links);
+      //set_images(response.data.images);
+  };
 
-    if (session.status === "loading") {
-        return (
-        <div className="fixed top-[25.5vh] left-[41.5vw]">
-        <img src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExY2N4eWw2c3F0dnRkcDJqMDNyNWdsanIxeHh4dmdudHRlZ2Z0dzB4YyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/zFwqvI1atpDkLPhBgY/giphy.gif"></img>
-        </div>
-      )
-      }
-      if(session.status==="unauthenticated"){
-        router.push("/signin")
-      }
-        console.log(validation)
-    return (
-    <div className="bg-[#181b2b] w-full h-full pl-4 text-white">
-    <div className="flex flex-row">
-        <div className="w-8/12 h-[98vh] rounded-xl border-[#8DECB4] border-2 ml-2 mt-2 mb-2 pl-2 pr-2 pt-2">
-        <div className="bg-[#2e3352] rounded-xl w-full h-[87vh] pl-2 pr-2 pb-2 text-lg overflow-y-auto">{validation}</div>
-        <button onClick={validate} className="bg-[#8DECB4] rounded-xl w-full h-16 mt-2 outline-none pl-2 max-h-16 min-h-16 text-xl text-black font-bold">Proceed</button>
-        </div>
-        <div className="w-4/12 max-h-[98vh] h-[98vh] rounded-xl border-[#8DECB4] border-2 mt-2 mb-2 ml-2 mr-2">
-        <iframe className="w-full rounded-t-xl h-80 border-b border-[#8DECB4]" src={`https://www.youtube.com/embed/${search}`} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <h2 className="pl-4 pt-4 pb-4 max-h-[60vh] overflow-y-auto text-lg">video descrioption checkin gwrap checkin gwrap checkin gwrap checkin gwrapcheckin gwrapcheckin gwrapcheckin checkin gwrap checkin gwrap checkin gwrap checkin gwrapcheckin gwrap checkin gwrap checkin gwrapcheckin gwrap checkin gwrap checkin gwrap checkin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapcheckin gwrapvgwrapcheckin gwrapcheckin gwrap</h2>
-        </div>
-    </div>
-    </div>
-    
-    );
+  if (session.status === "loading") {
+      return (
+          <div className="fixed top-[25.5vh] left-[41.5vw]">
+              <img src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExY2N4eWw2c3F0dnRkcDJqMDNyNWdsanIxeHh4dmdudHRlZ2Z0dzB4YyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/zFwqvI1atpDkLPhBgY/giphy.gif" alt="loading" />
+          </div>
+      );
+  }
+
+  if (session.status === "unauthenticated") {
+      router.push("/signin");
+  }
+
+  return (
+      <div className="bg-[#181b2b] w-full h-full pl-4 text-white">
+          <div className="flex flex-row">
+              <div className="w-8/12 h-[98vh] rounded-xl border-[#8DECB4] border-2 ml-2 mt-2 mb-2 pl-2 pr-2 pt-2">
+                  <div className="bg-[#2e3352] rounded-xl w-full h-[87vh] pl-2 pr-2 pb-2 text-lg overflow-y-auto">
+                      {!loading && <Home />}
+                      {loading && (
+                          <>
+                              <h2 className="p-5">{validation}</h2>
+                              <div className="grid grid-cols-2 gap-7 ml-4 mr-4">
+                                  {links.map((link, index) => (
+                                      <button key={index} className="h-[19.75vh] overflow-hidden bg-[#181b2b] border-[#8DECB4] border rounded-xl mt-2 hover:shadow-2xl transition-all duration-500 ease-in-out hover:scale-105" onClick={() => window.open(link.url, '_blank')}>
+                                          <div className={`flex flex-col justify-center text-white text-center rounded-tr-lg rounded-br-lg p-4`}>
+                                              <h2 className="text-lg mt-2">{link.title}</h2>
+                                              <p className="text-sm text-[#8DECB4] mt-2">{link.content}</p>
+                                          </div>
+                                      </button>
+                                  ))}
+                              </div>
+                              <div className="grid grid-cols-2 gap-7 ml-4 mr-4 mt-4">
+                                  {images.map((link, index) => (
+                                      <button key={index} className="h-auto w-auto overflow-hidden bg-[#181b2b] border-[#8DECB4] border rounded-xl mt-2 hover:shadow-2xl transition-all duration-500 ease-in-out hover:scale-105" onClick={() => window.open(link, '_blank')}>
+                                          <div className={`flex flex-col justify-center text-white text-center rounded-tr-lg rounded-br-lg p-4`}>
+                                              <button><img src={link} alt={`image_${index}`} /></button>
+                                          </div>
+                                      </button>
+                                  ))}
+                              </div>
+                          </>
+                      )}
+                  </div>
+                  <button onClick={validate} className="bg-[#8DECB4] rounded-xl w-full h-16 mt-2 outline-none pl-2 max-h-16 min-h-16 text-xl text-black font-bold active:bg-[#181b2b] active:border-[#8DECB4] active:border-2 active:text-[#8DECB4]">Proceed</button>
+              </div>
+              <div className="w-4/12 max-h-[98vh] h-[98vh] rounded-xl border-[#8DECB4] border-2 mt-2 mb-2 ml-2 mr-2">
+                  <iframe className="w-full rounded-t-xl h-80 border-b border-[#8DECB4]" src={`https://www.youtube.com/embed/${search}`} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowFullScreen title="YouTube video"></iframe>
+                  <button onClick={()=>{set_loading(false)}} className="mt-4 bg-[#8DECB4] ml-1 rounded-xl w-[27vw] h-16 mt-2 outline-none pl-2 max-h-16 min-h-16 text-xl text-black font-bold active:bg-[#181b2b] active:border-[#8DECB4] active:border-2 active:text-[#8DECB4]">+ New Video</button>
+              </div>
+          </div>
+      </div>
+  );
 };
 
 export default HomePage;
--- llm/main.py
+++ llm/main.py
@@ -1,19 +1,20 @@
-#uvicorn main:app --reload
-import asyncio
-import os
-from dotenv import load_dotenv
-from googlesearch import search
-from pyppeteer import launch
-from youtube_transcript_api import YouTubeTranscriptApi
-import google.generativeai as genai
+from typing import List
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from langchain_community.llms import Ollama
-import requests
+from youtube_transcript_api import YouTubeTranscriptApi
+from openai import OpenAI
 from tavily import TavilyClient
+from dotenv import load_dotenv
+import os
+from groq import Groq
 
+load_dotenv()
+groq_api_key = os.getenv("GROQ_API_KEY")
+tavily_api_key = os.getenv("TAVILY_API_KEY")
+client = Groq(api_key="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")
+tavily_client = TavilyClient(api_key=tavily_api_key)
 
-app=FastAPI()
+app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -22,20 +23,125 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.post("/api/guard-the-fact")
+@app.post("/api/guard-the-fact", response_model=dict)
 async def run(req_body: dict):
-    data = []
-    print(req_body["url"])
+    print("started")
+    final_data=[]
+    links=[]
+    images=[]
     transcript = YouTubeTranscriptApi.get_transcript(req_body["url"])
-    full = ""
-    for i in transcript:
-        full += i["text"] + " "
-    full="i woke up to a sad news that narendra modi passed away"
-    # load_dotenv()
-    llm = Ollama(model="llama2")
-    question = llm.invoke(f"generate a one liner question based on this context - {full}")
-    tavily = TavilyClient(api_key="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")
-    response = tavily.search(query=question, search_depth="advanced", include_answer=True,include_images=True)
-    #print(response)
-    final=llm.invoke(f"[true data]: {response['answer']} - [unsure data]:{full} - you are a fact validator known as FactGuard - compare the true data with unsure data and return if there are any factual inaccuracies")
-    return {"final":final,"links":response["results"],"images":response["images"]}
+    full_text = " ".join([i["text"] for i in transcript])
+
+    
+    completion = client.chat.completions.create(
+    model="llama3-70b-8192",
+    messages=[
+        {
+            "role": "system",
+            "content": "You are a part of video factual verification system your job is to extract the crucial information/ facts from the given transcript and return them as headlines. as we are trying to verify the facts told in the video it might be that there may be few or more factual inaccuracies throughout the transcript but that is not your job to look into those, your job is to just return the crucial information/facts that are given in the prompt as headlines. list out all such crucial information/facts from the transcript. Make sure that you return only the crucial information/facts which is essential to be verified for the good of the general public all the other things in the transcript can be ignored. each fact/information should have the complete context so don't use pronoun and use respective nouns always.list them in a squence seperated by @@@ no numbering or new lines"
+        },
+        {
+            "role": "user",
+            "content": full_text
+        }
+    ],
+    temperature=0.1,
+    max_tokens=256,
+    top_p=1,
+    stream=True,
+    stop=None,
+    )
+    text=""
+    for chunk in completion:
+        text+=chunk.choices[0].delta.content or ""
+    question_list = text.split("@@@")
+    counter=0
+    score=0
+    for question in question_list:
+        if question!="":
+            print(question)
+            counter+=1
+            completion = client.chat.completions.create(
+            model="llama3-70b-8192",
+            messages=[
+            {
+                "role": "system",
+                "content": "say 'yes' (lowercase) if the question is based on incident that happend on or before 2022, else say 'no' (lowercase)"
+            },
+            {
+                "role": "user",
+                "content": question
+            }
+            ],
+            temperature=0.1,
+            max_tokens=3,
+            top_p=1,
+            stream=True,
+            stop=None,
+            )
+            text2=""
+            for chunk in completion:
+                text2+=chunk.choices[0].delta.content or ""
+            print(text2)
+            if(text2=="no"):
+                print("verifying with tavily api ...")
+                tavily = TavilyClient(api_key=tavily_api_key)
+                response = tavily.search(query=question, search_depth="advanced", include_answer=True, include_images=True)
+                answer = response["answer"]
+                completion = client.chat.completions.create(
+                model="llama3-70b-8192",
+                messages=[
+                {
+                    "role": "system",
+                    "content": f"check for factual inaccuracies from the [verified data]:[] and find inaccuracies from [unverified data]:[]"
+                },
+                {
+                    "role": "user",
+                    "content": f"[verifed data]:{answer} - [unverified data]:{question}"
+                }
+                ],
+                temperature=0,
+                max_tokens=100,
+                top_p=1,
+                stream=True,
+                stop=None,
+                )
+                text4=""
+                for chunk in completion:
+                    text4+=chunk.choices[0].delta.content or ""
+                final_data.append(text4)
+    print("verifying with llama3 ...")
+    completion = client.chat.completions.create(
+    model="llama3-70b-8192",
+    messages=[
+                {
+                    "role": "system",
+                    "content": "is this data right or not ? if there are any factual inaccuracies, specify them with corrected data (keep the corrections short)"
+                },
+                {
+                    "role": "user",
+                    "content": full_text
+                }
+                ],
+                temperature=0.1,
+                max_tokens=256,
+                top_p=1,
+                stream=True,
+                stop=None,
+                )
+    text3=""
+    for chunk in completion:
+        text3+=chunk.choices[0].delta.content or ""
+    final_data.append(text3)
+    print()
+    print()
+    #print(final_data)
+    final_response=""
+    for data in final_data:
+        final_response+=data
+    return {
+        "score": 100,
+        "text": final_response,
+        "links":links,
+        "images":images
+    }
