41947a921e3be46331502e5d169eedb4763ce05f
spia-dm <srisiddhaarthp@gmail.com>
2024-05-30 13:43:23 +0530

    test

--- app/app/api/auth/[...nextauth]/route.tsx
+++ app/app/api/auth/[...nextauth]/route.tsx
@@ -4,8 +4,8 @@ import GoogleProvider from "next-auth/providers/google"
 const handler = NextAuth({
   providers:[
     GoogleProvider({
-        clientId:process.env.GOOGLE_CLIENT_ID,
-        clientSecret:process.env.GOOGLE_CLIENT_SECRET,
+        clientId: process.env.GOOGLE_CLIENT_ID || '', // Provide a default empty string
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET || '', // Provide a default empty string
     })
   ]
 })
